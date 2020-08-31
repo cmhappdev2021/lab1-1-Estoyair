78248593df85883982ab3e8e15cce9169f25d780
Estoyair <drhallevrda@gmail.com>
2020-08-31 18:27:53 +0000

    added some print lines

--- main.swift
+++ main.swift
@@ -7,8 +7,12 @@ print("How to use playgrounds to make writing Swift fun and simple")
 
 // Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the print function to display the song title and artist.
 
-
+print("shinobi no kami")
+print("by rustage")
 
 // Use multiple print functions to write out some of the lyrics to the song.
 
-
+print("ill leave them all sticky like a sycamore")
+print("ill sic them all cause")
+print("stick it to ya stigmas cause")
+print("im sick of war")
